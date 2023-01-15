f2d9eabe6a5973abc873d1174d6227c17d450944
mycomputer <Danil12.07.2012@gail.com>
2023-01-15 13:09:43 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задача 1");
-        int salary = 15000;
-        int total = 0;
-        int i = 0;
-        while (total <= 2_459_000) {
-            i++;
-            total = total + total/100*12;
-            total = total + salary;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + total + "рублей.");
+        System.out.println("Задача 2");
+        int a = 0;
+        while (a < 10) {
+            a++;
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        a = a + 1;
+        for ( ; a>1; ) {
+            a--;
+            System.out.print(a + " ");
         }
         }
 
