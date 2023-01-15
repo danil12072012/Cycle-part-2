e4c27f3e15ddd5d235b6f34904fcea4c16af7352
mycomputer <Danil12.07.2012@gail.com>
2023-01-15 13:34:59 +0300

    Task 6

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задача 5");
+        System.out.println("Задача 6");
        int deposiAmount = 15000;
        int percent = 7;
-       for (int i =0; deposiAmount <= 12_000_000; i++){
+       int time = 9;
+       for (int i =0; i <= time*12; i++){
            deposiAmount = deposiAmount + deposiAmount/100*percent;
            if (i %6 == 0){
            System.out.println("Месяц " + i + ", сумма накоплений " + deposiAmount);
