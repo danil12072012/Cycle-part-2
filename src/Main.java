b60f01a1deb57266d61543326a6b58c6b73a9347
mycomputer <Danil12.07.2012@gail.com>
2023-01-15 13:57:49 +0300

    Task 7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задача 6");
-       int deposiAmount = 15000;
-       int percent = 7;
-       int time = 9;
-       for (int i =0; i <= time*12; i++){
-           deposiAmount = deposiAmount + deposiAmount/100*percent;
-           if (i %6 == 0){
-           System.out.println("Месяц " + i + ", сумма накоплений " + deposiAmount);
+        System.out.println("Задача 7");
+       int i = 3;
+       while ( i < 31) {
+           i=i+7;
+
+           System.out.println("Сегодня пятница " + i + "-е число. Необходимо подготовить отчет.");
+
         }
         }
 
-    }}
+    }
