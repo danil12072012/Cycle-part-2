22328f19a68896e319ad98b0304e25fd5aceb715
mycomputer <Danil12.07.2012@gail.com>
2023-01-15 13:19:51 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задача 2");
-        int a = 0;
-        while (a < 10) {
-            a++;
-            System.out.print(a + " ");
-        }
-        System.out.println();
-        a = a + 1;
-        for ( ; a>1; ) {
-            a--;
-            System.out.print(a + " ");
+        System.out.println("Задача 3");
+       int size = 12_000_000;
+       int birthRate = 17;
+       int mortality = 8;
+       for (int i=1; i<=10; i++){
+           size = size + size/1000*(birthRate-mortality);
+           System.out.println("Год " + i + ", численость населения составляет " + size);
         }
         }
 
