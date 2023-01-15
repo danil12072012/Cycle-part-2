54b99741d6209efbd29e9a8b3de84303bf2cff0f
mycomputer <Danil12.07.2012@gail.com>
2023-01-15 12:58:15 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задача 1");
+        int salary = 15000;
+        int total = 0;
+        int i = 0;
+        while (total < 2_459_000) {
+            i++;
+            total = total + total/100*12;
+            total = total + salary;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + "рублей.");
+        }
+        }
 
     }
-}
